16f006369f70a4772ab6e48bdd124ec027590b1f
Brian Alexander <brian.peralta@engie.com>
2021-05-01 15:24:37 +0800

    Implementation of function;

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,27 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let component: AppComponent;
+
+  beforeEach(() => {
+    component = new AppComponent();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  describe('given a list of numerical strings, it should give you the 2nd unique max...', () => {
+    it('with [3, -2], it should return -2', () => {
+      expect(component.find2ndMax(['3', '-2'])).toEqual('-2');
+    });
 
-  it(`should have as title 'uploan-exam'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('uploan-exam');
-  });
+    it('with [5, 5, 4, 2], it should return 4', () => {
+      expect(component.find2ndMax(['5', '5', '4', '2'])).toEqual('4');
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('uploan-exam app is running!');
+    it('with [4, 4, 4], it should return -1', () => {
+      expect(component.find2ndMax(['4', '4', '4'])).toEqual('-1');
+    });
+
+    it('with [], it should return -1', () => {
+      expect(component.find2ndMax([])).toEqual('-1');
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'uploan-exam';
+  find2ndMax(numbersInString: string[]): string {
+    const uniqueized = Array.from(new Set(numbersInString));
+    const sorted = uniqueized.reverse();
+    const toBeReturned = sorted.length > 1 ? sorted[sorted.length - 2] : -1;
+    return toBeReturned.toString();
+  }
 }
