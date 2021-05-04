5ccc06491f369045c119f5bc8e6c603cbab048ed
baraluga <baperaltaa@gmail.com>
2021-05-05 07:26:15 +0800

    Removed unused imports;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { numberSortAscending } from 'num-sort';
 import randomInteger from 'random-int';
 import { BehaviorSubject } from 'rxjs';
 
